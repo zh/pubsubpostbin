c9e3099e1058ef44d36d0b74b3a911deaeb2f136
Stoyan Zhekov <zh@zhware.net>
2009-08-12 16:11:13 +0900

    classy sinatra app, not needed requirements removed

--- config.ru
+++ config.ru
@@ -1,9 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'sequel'
 
-Sinatra::Application.set :run => false
-Sinatra::Application.set :environment => ENV['RACK_ENV']
-
-require 'pubsubpostbin.rb'
-run Sinatra::Application
+require 'pubsubpostbin'
+run PubSubPostBin::App
--- pubsubpostbin.rb
+++ pubsubpostbin.rb
@@ -1,8 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'sequel'
-require 'zlib'
-require 'json'
 require 'crack'
 require 'httpclient'
 
@@ -14,67 +11,77 @@ rescue
   MyTimer = Timeout
 end
 
-configure do
-  GIVEUP = 10
-end
+module PubSubPostBin
+  class App < Sinatra::Default
 
-helpers do
-  def atom_parse(text)
-    atom = Crack::XML.parse(text)
-    r = []
-    if atom["feed"]["entry"].kind_of?(Array)
-      atom["feed"]["entry"].each { |e| 
-        r << {:id => e["id"], :title => e["title"], :published => e["published"] }
-      }
-    else
-      e = atom["feed"]["entry"]
-      r = {:id => e["id"], :title => e["title"], :published => e["published"] }
+    set :sessions, false
+    set :run, false
+    set :environment, ENV['RACK_ENV']
+
+    configure do
+      GIVEUP = 10
     end
-    r
-  end
-
-  def postman(url, msg)
-    msg = [msg].flatten  # convert single entry to array
-    msg.each do |m|
-      begin
-        MyTimer.timeout(GIVEUP) do
-          HTTPClient.post(url, m)
+    
+    helpers do
+      def atom_parse(text)
+        atom = Crack::XML.parse(text)
+        r = []
+        if atom["feed"]["entry"].kind_of?(Array)
+          atom["feed"]["entry"].each { |e| 
+            r << {:id => e["id"], :title => e["title"], :published => e["published"] }
+          }
+        else
+          e = atom["feed"]["entry"]
+          r = {:id => e["id"], :title => e["title"], :published => e["published"] }
         end
-      rescue Exception => e
-        case e
-          when Timeout::Error
-            puts "Timeout: #{sub}"
-          else  
-            puts e.to_s
-        end  
-        next
+        r
       end
+    
+      def postman(url, msg)
+        msg = [msg].flatten  # convert single entry to array
+        msg.each do |m|
+          begin
+            MyTimer.timeout(GIVEUP) do
+              HTTPClient.post(url, m)
+            end
+          rescue Exception => e
+            case e
+              when Timeout::Error
+                puts "Timeout: #{sub}"
+              else  
+                puts e.to_s
+            end  
+            next
+          end
+        end
+      end
+    
     end
+    
+    get '/' do
+      erb :index
+    end
+    
+    # Superfeedr
+    get '/superfeedr/:secret/:id' do
+      return params[:secret]
+    end
+    
+    post '/superfeedr/:secret/:id' do
+      msg = atom_parse(request.body.string)
+      postman("http://postbin.org/#{params[:id]}", msg)
+    end 
+    
+    
+    # PubSubHubbub
+    get '/:id' do
+      return request['hub.challenge']
+    end  
+    
+    post '/:id' do
+      msg = atom_parse(request.body.string)
+      postman("http://www.postbin.org/#{params[:id]}", msg)
+    end
+
   end
-
-end
-
-get '/' do
-  erb :index
-end
-
-# Superfeedr
-get '/superfeedr/:secret/:id' do
-  return params[:secret]
-end
-
-post '/superfeedr/:secret/:id' do
-  msg = atom_parse(request.body.string)
-  postman("http://postbin.org/#{params[:id]}", msg)
-end 
-
-
-# PubSubHubbub
-get '/:id' do
-  return request['hub.challenge']
 end  
-
-post '/:id' do
-  msg = atom_parse(request.body.string)
-  postman("http://www.postbin.org/#{params[:id]}", msg)
-end 
